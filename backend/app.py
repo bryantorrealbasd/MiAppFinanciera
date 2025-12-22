b74417568539564c611c1aa726f3881370072a2e
bryantorrealbasd <bryantorrealba.sd@gmail.com>
2025-12-22 01:57:15 -0400

    Arquitectura final funcional con soporte

--- backend/app.py
+++ backend/app.py
@@ -138,6 +138,9 @@ DASHBOARD_HTML = '''
 </body>
 </html>
 '''
+
+# --- ARRANQUE DEL SERVIDOR ---
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
